4e67820ae0df170f1be7f84d18c6b32a2174ab97
Seonjin Na <monokai@kaist.ac.kr>
2024-12-16 12:07:06 -0500

    update default color

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -185,10 +185,11 @@ let transTheme = () => {
 
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
-    const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      theme = "dark";
-    }
+    // const userPref = window.matchMedia;
+    theme = "light"
+    // if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+    //   theme = "dark";
+    // }
   }
 
   setTheme(theme);
